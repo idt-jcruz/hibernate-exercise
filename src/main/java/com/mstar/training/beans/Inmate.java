e01e8864c98484256af23f752b5b9523d6345fd2
Jason Cruz <jason@ideyatech.com>
2017-07-24 14:37:17 +0800

    Add inmate class

---
+++ src/main/java/com/mstar/training/beans/Inmate.java
@@ -0,0 +1,104 @@
+package com.mstar.training.beans;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Inmate {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "INMATE_NAME")
+	private String name;
+
+	@Column(name = "CONTROL_NUMBER", nullable = false)
+	private String controlNumber;
+
+	private String inmateStatus;
+	
+	@ManyToOne
+	@JoinColumn(name = "JAIL_ID", nullable = false)
+	private Jail inmateJail;
+
+	@Column(name = "COMMITMENT_DATE", nullable = false)
+	private Date commitmentDate;
+
+	@Column(name = "END_DATE")
+	private Date endDate;
+	
+	private int sentenceLength;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getControlNumber() {
+		return controlNumber;
+	}
+
+	public void setControlNumber(String controlNumber) {
+		this.controlNumber = controlNumber;
+	}
+
+	public Jail getJail() {
+		return inmateJail;
+	}
+
+	public void setJail(Jail jail) {
+		this.inmateJail = jail;
+	}
+
+	public Date getCommitmentDate() {
+		return commitmentDate;
+	}
+
+	public void setCommitmentDate(Date commitmentDate) {
+		this.commitmentDate = commitmentDate;
+	}
+
+	public Jail getInmateJail() {
+		return inmateJail;
+	}
+
+	public void setInmateJail(Jail inmateJail) {
+		this.inmateJail = inmateJail;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getInmateStatus() {
+		return inmateStatus;
+	}
+
+	public void setInmateStatus(String inmateStatus) {
+		this.inmateStatus = inmateStatus;
+	}
+
+}
